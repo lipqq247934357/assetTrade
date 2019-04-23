fd37cda83b44c7332bfb927a2faf7fa964da0560
lipeng <247934357@qq.com>
2019-04-23 16:31:00 +0800

    增加获取url上search的方法

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,6 +6,24 @@
  * 备注：vue-router中的this.$route.query可以获取到需要的内容
  *
  */
+
+export const urlParse4Search = function () {
+    let url = window.location.search;
+    let obj = {};
+    let reg = /[?&][^?&]+=[^?&]+/g;
+    let arr = url.match(reg);
+    // ['?id=12345', '&a=b']
+    if (arr) {
+        arr.forEach((item) => {
+            let tempArr = item.substring(1).split('=');
+            let key = decodeURIComponent(tempArr[0]);
+            let val = decodeURIComponent(tempArr[1]);
+            obj[key] = val;
+        });
+    }
+    return obj;
+};
+
 export const urlParse = function () {
     let url = window.location.hash;
     let obj = {};
