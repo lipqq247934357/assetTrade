1359e6f297544ebfafb7ca5d8f6afdf1d89ad00b
lipeng <247934357@qq.com>
2019-04-25 10:48:23 +0800

    修改首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -5,16 +5,15 @@
 </template>
 
 <script>
-    import {mapGetters} from 'vuex'
 
     export default {
         name: 'Dashboard',
-        computed: {
-            ...mapGetters([
-                'name',
-                'roles'
-            ])
-        }
+        data() {
+            return {}
+        },
+        created() {
+        },
+        methods: {}
     }
 </script>
 
