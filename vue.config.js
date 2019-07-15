4e95a412b69ea96c84bfe1bbc0116a73e79cb364
lipeng <247934357@qq.com>
2019-07-15 17:14:57 +0800

    修改vueConfig和依赖

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,7 @@ module.exports = {
             '/AssetWeb': {
                 // target: 'http://172.16.131.67:8005',
                 target: 'http://172.16.131.31:8005', // 测试环境
+                // target: 'http://10.89.2.119:8005', // 测试环境
                 // target: 'http://10.192.86.12:8005',
 
                 ws: true,
