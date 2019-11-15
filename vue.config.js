1ad84c46c84eb19717110a75a51509095fb0dcb4
lipeng <247934357@qq.com>
2019-11-15 19:15:55 +0800

    更新npm依赖，修改vue代理配置

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,17 @@
 
 module.exports = {
     // 选项...
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/assettrade/'
+        : '/',
     devServer: {
         // before(app) {
         //     mock(app);
         // },
         proxy: {
             '/AssetWeb': { //资产管理系统
-                // target: 'http://172.16.131.31:8005', // 本地1
-                target: 'http://172.16.131.31:8005', // 测试环境
+                target: 'http://172.16.131.31:7100', // 本地1
+                // target: 'http://172.16.131.31:8005', // 测试环境
                 ws: true,
                 changeOrigin: true
             },
@@ -21,6 +24,15 @@ module.exports = {
                 // target: 'http://10.89.2.119:7205', // 本地1
                 target: 'http://172.16.131.31:8000', // 测试环境
                 ws: true,
+                changeOrigin: true
+            },
+            '/batch-guar': { //担保代偿系统
+                // target: 'http://10.89.2.119:7290', // 本地1
+                target: 'http://172.16.131.31:7290', // 测试环境
+                ws: true,
+                pathRewrite: {
+                    '^/batch-guar': ''
+                },
                 changeOrigin: true
             },
             '/ucenter': { // 用户中心
