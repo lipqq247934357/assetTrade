234deb219c99fe48bda9513d07bfebe2484098d5
lipeng <247934357@qq.com>
2019-12-09 10:48:43 +0800

    修改担保代偿详情页面

--- src/views/grtCpsM/grtCps/grtdetail.vue
+++ src/views/grtCpsM/grtCps/grtdetail.vue
@@ -1,92 +1,75 @@
 <template>
-    <div class="detail app-container">
-        <div :class="$style['detail-list']">
-            <div :class="$style['content-table']">
-                <el-table
-                        :data="data"
-                        border
-                        empty-text="无数据"
-                        header-cell-class-name="header-cell-class-name"
-                        size="medium"
-                        style="width: 100%">
-                    <el-table-column
-                            label="借据号"
-                            min-width="150px"
-                            prop="LISTID">
-                    </el-table-column>
-                    <el-table-column label="期次" prop="TERM">
-                    </el-table-column>
-                    <el-table-column label="应还日期" prop="SDATE">
-                    </el-table-column>
-                    <el-table-column label="应还本金" prop="SCAPI">
-                    </el-table-column>
-                    <el-table-column label="应还利息" prop="SINTE">
-                    </el-table-column>
-                    <el-table-column label="应收担保费" prop="SASSUREAMT">
-                    </el-table-column>
-                    <el-table-column label="实还日期" prop="RDATE">
-                    </el-table-column>
-                    <el-table-column label="实还本金" prop="RCAPI">
-                    </el-table-column>
-                    <el-table-column label="实还利息" prop="RINTE">
-                    </el-table-column>
-                    <el-table-column label="实收担保费" prop="RASSUREAMT">
-                    </el-table-column>
-                </el-table>
-            </div>
+  <div class="detail app-container">
+    <div :class="$style['detail-list']">
+      <div :class="$style['content-table']">
+        <el-table
+          :data="data"
+          border
+          empty-text="无数据"
+          header-cell-class-name="header-cell-class-name"
+          size="medium"
+          style="width: 100%"
+        >
+          <el-table-column label="借据号" min-width="150px" prop="LISTID"></el-table-column>
+          <el-table-column label="期次" prop="TERM"></el-table-column>
+          <el-table-column label="应还日期" prop="SDATE"></el-table-column>
+          <el-table-column label="应还本金" prop="SCAPI"></el-table-column>
+          <el-table-column label="应还利息" prop="SINTE"></el-table-column>
+          <el-table-column label="应收担保费" prop="SASSUREAMT"></el-table-column>
+          <el-table-column label="实还日期" prop="RDATE"></el-table-column>
+          <el-table-column label="实还本金" prop="RCAPI"></el-table-column>
+          <el-table-column label="实还利息" prop="RINTE"></el-table-column>
+          <el-table-column label="实收担保费" prop="RASSUREAMT"></el-table-column>
+        </el-table>
+      </div>
 
-            <div style="width: 100%;text-align: center;padding-top: 30px;">
-                <el-button @click="back" size="small" style="width: 100px;" type="primary">
-                    <span style="font-size: 14px;">返&nbsp;回</span>
-                </el-button>
-            </div>
-        </div>
+      <div style="width: 100%;text-align: center;padding-top: 30px;">
+        <el-button @click="back" size="small" style="width: 100px;" type="primary">
+          <span style="font-size: 14px;">返&nbsp;回</span>
+        </el-button>
+      </div>
     </div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import formatter from "@/components/mixins/formatter";
 
-    import formatter from '@/components/mixins/formatter';
-
-    export default {
-        name: 'assetDetail',
-        mixins: [formatter],
-        data() {
-            return {
-                data: []
-            }
-        },
-        activated() {
-            let listId = this.$route.query.listId;
-            let channelId = this.$route.query.channelId;
-            if (listId && channelId) {
-                this.getInfo(listId, channelId);
-            } else {
-                this.$message.warning({message: '参数异常', duration: 2000});
-                setTimeout(() => {
-                    this.back();
-                }, 1000)
-            }
-        },
-        methods: {
-            async getInfo(listId, channelId) { // 根据id获取数据
-                let res = await this.$api.grtCps.billsDetail({listId, channelId});
-                this.data = res.data.data;
-            },
-            back() {
-                this.$router.go(-1);
-            }
-        }
+@Component({
+  name: "assetDetail",
+  mixins: [formatter]
+})
+export default class extends Vue {
+  data: object[] = [];
+  activated() {
+    let listId = this.$route.query.listId;
+    let channelId = this.$route.query.channelId;
+    if (listId && channelId) {
+      this.getInfo(listId + "", channelId + "");
+    } else {
+      this.$message.warning({ message: "参数异常", duration: 2000 });
+      setTimeout(() => {
+        this.back();
+      }, 1000);
     }
-
-
+  }
+  async getInfo(listId: string, channelId: string) {
+    // 根据id获取数据
+    let res = await this.$api.grtCps.billsDetail({ listId, channelId });
+    this.data = res.data.data;
+  }
+  back() {
+    this.$router.go(-1);
+  }
+}
 </script>
 
 <style lang="scss" module>
-    @import '../../../styles/common/asset-detail';
+@import "../../../styles/common/asset-detail";
 
-    .content-table {
-        width: 90%;
-        margin: 0 auto 20px;
-    }
+.content-table {
+  width: 90%;
+  margin: 0 auto 20px;
+}
 </style>
